af5b699a7982c4a6f3af4d9f0cc416682c378294
ZanziPanziBY <zanzipanziby@gmail.com>
2024-06-02 15:20:53 +0300

    feat: add basic test code

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import {createServer} from 'http'
+import {Server} from 'socket.io';
+import cors from 'cors'
+import {router} from "./route.js";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors({origin: '*'}));
+app.use(router)
+const server = createServer(app)
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	}
+})
+
+
+server.listen(PORT, () => console.log('Server started on port 5000'));
---
+++ src/route.ts
@@ -0,0 +1,7 @@
+import {Router, Request, Response} from "express";
+
+export const router = Router();
+
+router.get("/", (_: Request, res: Response) => {
+	res.send("Hello World!");
+})
